590e70ea73837649baadaeff6a01a98f44f40b21
OvalSky <ovalsky@users.noreply.github.com>
2018-12-03 18:26:23 +0700

    Update MWTapDetectingImageView.m

--- ios/MWTapDetectingImageView.m
+++ ios/MWTapDetectingImageView.m
@@ -31,38 +31,47 @@
 	return self;
 }
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-	UITouch *touch = [touches anyObject];
-	NSUInteger tapCount = touch.tapCount;
-	switch (tapCount) {
-		case 1:
-			[self handleSingleTap:touch];
-			break;
-		case 2:
-			[self handleDoubleTap:touch];
-			break;
-		case 3:
-			[self handleTripleTap:touch];
-			break;
-		default:
-			break;
-	}
-	[[self nextResponder] touchesEnded:touches withEvent:event];
+- (void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    if(touch.tapCount > 1){
+        [NSObject cancelPreviousPerformRequestsWithTarget:self
+                                                 selector:@selector(handleSingleTap:)
+                                                   object:nil];
+    }
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    self.touch = touch;
+    switch (touch.tapCount) {
+        case 1:
+            [self performSelector:@selector(handleSingleTap:) withObject:nil afterDelay:0.4];
+            break;
+        case 2:
+            [self performSelector:@selector(handleDoubleTap:) withObject:touch];
+            break;
+        case 3:
+            [self performSelector:@selector(handleTripleTap:) withObject:touch];
+            break;
+        default:
+            break;
+    }
+    [[self nextResponder] touchesEnded:touches withEvent:event];
 }
 
 - (void)handleSingleTap:(UITouch *)touch {
-	if ([_tapDelegate respondsToSelector:@selector(imageView:singleTapDetected:)])
-		[_tapDelegate imageView:self singleTapDetected:touch];
+    if ([_tapDelegate respondsToSelector:@selector(imageView:singleTapDetected:)])
+        [_tapDelegate imageView:self singleTapDetected:touch];
 }
 
 - (void)handleDoubleTap:(UITouch *)touch {
-	if ([_tapDelegate respondsToSelector:@selector(imageView:doubleTapDetected:)])
-		[_tapDelegate imageView:self doubleTapDetected:touch];
+    if ([_tapDelegate respondsToSelector:@selector(imageView:doubleTapDetected:)])
+        [_tapDelegate imageView:self doubleTapDetected:touch];
 }
 
 - (void)handleTripleTap:(UITouch *)touch {
-	if ([_tapDelegate respondsToSelector:@selector(imageView:tripleTapDetected:)])
-		[_tapDelegate imageView:self tripleTapDetected:touch];
+    if ([_tapDelegate respondsToSelector:@selector(imageView:tripleTapDetected:)])
+        [_tapDelegate imageView:self tripleTapDetected:touch];
 }
 
 @end
